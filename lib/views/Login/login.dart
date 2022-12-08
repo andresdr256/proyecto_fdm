cb3ac7dfce588cfd02b4560aabcb5778bc20d7df
Andrés De la rosa <55836814+andresdr256@users.noreply.github.com>
2022-12-08 14:50:45 -0600

    Upload login view

---
+++ lib/views/Login/login.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:travelappui/constants/constants.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    ThemeData appTheme = Theme.of(context);
+    return Scaffold(backgroundColor: kPrimaryColor, body: Text("Login Screen"));
+  }
+}
